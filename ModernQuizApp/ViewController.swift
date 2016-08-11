1d3f777f2e048eef5ad50079f7fb427ea280a76a
Paul Maxeiner <paulmaxeiner@pauls-macbook-air.local>
2016-08-11 17:05:14 -0400

    Initial Commit

---
+++ ModernQuizApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModernQuizApp
+//
+//  Created by Paul Maxeiner on 8/11/16.
+//  Copyright © 2016 Paul Maxeiner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
