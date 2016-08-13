e158fe792c7c0b8e3eec479c819be11de5bfbc50
Paul Maxeiner <paulmaxeiner@pauls-macbook-air.local>
2016-08-13 12:07:14 -0500

    Added More ViewControllers

---
+++ ModernQuizApp/MainScreenViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainScreenViewController.swift
+//  ModernQuizApp
+//
+//  Created by Paul Maxeiner on 8/13/16.
+//  Copyright © 2016 Paul Maxeiner. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+    @IBOutlet weak var yesTitleLabel: UILabel!
+    @IBOutlet weak var noTitleLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
